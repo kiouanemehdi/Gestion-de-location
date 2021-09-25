cff5e9269fc43690ab2425d41be0dfc279ac5ecf
El Mehdi Kiouane <30392394+kiouanemehdi@users.noreply.github.com>
2021-09-25 19:07:54 +0200

    ajout de la page accueil.php avec un nouveau design

---
+++ php/Accueil.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title> Home </title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/all.css">	
+    <link rel="stylesheet" href="../css/fontawesome.min.css">
+  
+     <link rel="stylesheet" href="../css/Accueil_style.css">
+</head>
+
+<body>
+	<header style="position: relative;">
+        <div style="background-color: #e3fcf6; height: 100px;" class="px-3 py-2 text-white">
+            <div class="container">
+
+                 <div id="logo" style="position: absolute;margin-top: -20px;">
+                     <img style="" src="../img/logo2.png" height="130px" width="130px">
+                 </div>
+                                
+                           
+                <div style="position: absolute;" id="menu" class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <!-- <div  class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+                        <h1>EILCO CAR RENTAL</h1>
+                    </div>-->
+                   
+                        <ul id="nav" class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+
+                            <li class="menu-hover">
+                                <a href="voitures.php" class="nav-link " style="color: #2BD0AE;">
+                                    <i style="color: #2BD0AE;" class="fas fa-car fa-3x"></i><br>
+                                    <p style="font-weight: bold;">Voitures</p>
+                                </a>
+                            </li>
+                            <li class="menu-hover">
+                                 <a href="clients.php" class="nav-link " style="color: #2BD0AE;">
+                                    <i style="color: #2BD0AE;" class="fas fa-user fa-3x"></i><br>
+                                    <p style="font-weight: bold;">Clients</p>
+                                </a>
+                            </li>
+                            <li class="menu-hover">
+                                 <a href="documents.php" class="nav-link " style="color: #2BD0AE;">
+                                    <i style="color: #2BD0AE;" class="fas fa-key fa-3x"></i><br>
+                                   <p style="font-weight: bold;"> Locations</p>
+                                </a>
+                            </li>
+                            <li class="menu-hover">
+                                 <a href="reservations.php" class="nav-link " style="color: #2BD0AE;">
+                                    <i style="color: #2BD0AE;" class="fas fa-undo fa-3x"></i><br>
+                                    <p style="font-weight: bold;">Retourne</p>
+                                </a>
+                            </li>
+                             <li>
+                                 <a id="log_out_logo" href="#" class="nav-link" style="color: #d30038;">
+                                    <i style="color: #d30038;" class="fas fa-sign-out-alt fa-3x"></i><br>
+                                    <p style="font-weight: bold;"> Deconnexion</p>
+                                </a>
+                            </li>
+
+                        </ul>
+                                
+                </div>
+            </div>
+        </div>
+        
+    </header>
+
+	<script src="../js/main.js"></script>
+	<script src="../js/popper.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<script src="../js/all.js"></script>
+
+
+</body>
+</html>
+
