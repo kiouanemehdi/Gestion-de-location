7554c04fd2aeef2ccccdd2ab25f5bb4622570c67
ahmedsabouni <74497842+ahmedsabouni@users.noreply.github.com>
2021-09-25 11:54:50 +0200

    nouvelle base de donnée

--- database/location.sql
+++ database/location.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 24 sep. 2021 à 12:02
+-- Généré le : sam. 25 sep. 2021 à 11:53
 -- Version du serveur :  10.4.18-MariaDB
 -- Version de PHP : 7.3.27
 
@@ -28,23 +28,36 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `client` (
+  `id_client` int(100) NOT NULL,
   `Nom` varchar(15) NOT NULL,
   `Prenom` varchar(15) NOT NULL,
   `tel` varchar(15) NOT NULL,
-  `id_client` int(100) NOT NULL,
   `mail` varchar(30) NOT NULL,
   `adresse` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`id_client`, `Nom`, `Prenom`, `tel`, `mail`, `adresse`) VALUES
+(1, 'sabouni', 'ahmed', '0276355360', 'ahmed;sabouni11@gmail.com', '13 Boulevard leon gambetta'),
+(2, 'mehdi', 'el kiouane ', '0367537638', 'mehdi.kiounae@gmail.com', '17 Boulevard dchira'),
+(3, 'nadini', 'aya', '0276537637', 'aya.nadini17@gmail.com', 'Doukala');
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `marque`
+-- Structure de la table `reservation`
 --
 
-CREATE TABLE `marque` (
-  `id_marque` int(50) NOT NULL,
-  `marque` varchar(40) NOT NULL
+CREATE TABLE `reservation` (
+  `id_reservation` int(50) NOT NULL,
+  `id_client` int(200) NOT NULL,
+  `id_voiture` int(200) NOT NULL,
+  `date_location` date NOT NULL,
+  `date_retour` date NOT NULL,
+  `montant` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -54,14 +67,21 @@ CREATE TABLE `marque` (
 --
 
 CREATE TABLE `voiture` (
-  `id` int(30) NOT NULL,
-  `matricule` varchar(30) NOT NULL,
-  `Modele` varchar(30) NOT NULL,
-  `Prix_location` int(30) NOT NULL,
-  `Date_Sortie` date NOT NULL,
-  `Categorie` varchar(30) NOT NULL,
-  `id_marque` int(200) NOT NULL
+  `id_voiture` int(11) NOT NULL,
+  `matricule` varchar(20) NOT NULL,
+  `model` varchar(20) NOT NULL,
+  `marque` varchar(20) NOT NULL,
+  `categorie` varchar(20) NOT NULL,
+  `prix_location` int(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `voiture`
+--
+
+INSERT INTO `voiture` (`id_voiture`, `matricule`, `model`, `marque`, `categorie`, `prix_location`) VALUES
+(1, 'zh672BZJ', 'clio', 'renault', 'citadine', 30),
+(2, 'HJ672JE', 'model 3', 'tesla', 'berline', 50);
 
 --
 -- Index pour les tables déchargées
@@ -74,17 +94,17 @@ ALTER TABLE `client`
   ADD PRIMARY KEY (`id_client`);
 
 --
--- Index pour la table `marque`
+-- Index pour la table `reservation`
 --
-ALTER TABLE `marque`
-  ADD PRIMARY KEY (`id_marque`);
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id_reservation`);
 
 --
 -- Index pour la table `voiture`
 --
 ALTER TABLE `voiture`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_marque` (`id_marque`);
+  ADD PRIMARY KEY (`id_voiture`),
+  ADD UNIQUE KEY `matricule` (`matricule`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -94,29 +114,19 @@ ALTER TABLE `voiture`
 -- AUTO_INCREMENT pour la table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id_client` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_client` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT pour la table `marque`
+-- AUTO_INCREMENT pour la table `reservation`
 --
-ALTER TABLE `marque`
-  MODIFY `id_marque` int(50) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `reservation`
+  MODIFY `id_reservation` int(50) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `voiture`
 --
 ALTER TABLE `voiture`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `voiture`
---
-ALTER TABLE `voiture`
-  ADD CONSTRAINT `voiture_ibfk_1` FOREIGN KEY (`id_marque`) REFERENCES `marque` (`id_marque`);
+  MODIFY `id_voiture` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
